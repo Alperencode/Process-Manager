8b6848153d3ed595c458be40c72694c563221d4c
Alperen Aga <alperencode@gmail.com>
2023-03-19 11:33:45 +0300

    Add fork() test

--- Main.c
+++ Main.c
@@ -1,5 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+
 int main(){
 
+    // First child process
+    int pid = fork();
+
+    if(pid==0){
+        // Second child process under first child
+        int pid2 = fork();
+        printf("pid: %d pid2: %d\n", pid, pid2);
+    }else
+        printf("pid: %d\n", pid);
+
+    while(1);
     return 0;
 
 }
